c13b27ea55217d103c946cf0708f60e07aabbff1
Abhishek sharma <92371621+Abhisharmanan8312@users.noreply.github.com>
2022-11-14 12:57:43 +0530

    Add files via upload

---
+++ Switch Dark Theme/index.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("input");
+const body = document.querySelector("body");
+const toggleThemeMode = () => {
+    body.classList.toggle("dark");
+};
+input.onchange = toggleThemeMode; 
