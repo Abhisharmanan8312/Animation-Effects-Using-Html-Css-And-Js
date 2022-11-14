e21ddc1af260ade4bc84206ab92d51a70628ec1c
Abhishek sharma <92371621+Abhisharmanan8312@users.noreply.github.com>
2022-11-14 12:59:06 +0530

    Delete Switch Dark Theme directory

--- Switch Dark Theme/index.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector("input");
-const body = document.querySelector("body");
-const toggleThemeMode = () => {
-    body.classList.toggle("dark");
-};
-input.onchange = toggleThemeMode; 
