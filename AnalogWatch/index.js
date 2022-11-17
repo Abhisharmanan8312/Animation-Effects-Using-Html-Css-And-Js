4b2c1e83450bc09bf945c7c15bc018492f32871d
Abhishek sharma <92371621+Abhisharmanan8312@users.noreply.github.com>
2022-11-17 09:19:22 +0530

    Add files via upload

---
+++ AnalogWatch/index.js
@@ -0,0 +1,17 @@
+const currentTime = () => {
+    const clockTime = document.querySelector(".clock-time");
+    const markingSeconds = document.querySelector(".marking-seconds");
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    hh = hh < 10 ? `0${hh}` : hh;
+    mm = mm < 10 ? `0${mm}` : mm;
+    ss = ss < 10 ? `0${ss}` : ss;
+    const ssdegress = (ss / 60) * 360;
+    markingSeconds.style.transform = `rotate(${ssdegress}deg)`;
+    let time = `${hh}:${mm}`;
+    clockTime.innerText = time;
+};
+currentTime();
+setInterval(currentTime, 1000);
