f08b77f9768f7418cb5cfbdbdc6ee6a017097068
Abhishek sharma <92371621+Abhisharmanan8312@users.noreply.github.com>
2022-12-12 10:31:53 +0530

    Add files via upload

---
+++ AnalogWatch2/index.js
@@ -0,0 +1,13 @@
+var deg = 6;
+const hr = document.querySelector('.hour-hand');
+const mn = document.querySelector('.min-hand');
+const sc = document.querySelector('.sec-hand');
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
